6d6e62e0dda131524600312a9131b396b4aa6ef2
Uldis Silins <v-ulsili@microsoft.com>
2017-04-25 15:54:34 +0300

    MD 1 finished

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,8 +1,28 @@
 When(/^I am on Appimation login page/) do
   visit('/')
-  all(:xpath, "//h3[contains(text(), 'Chain requests with reusable data')]/parent::section/descendant::img").each do |el|
+#  all(:xpath, "//h3[contains(text(), 'Chain requests with reusable data')]/parent::section/descendant::img").each do |el|
+#    puts el
+#  end
+
+  find(:xpath,'//*[@id="start_button"]')
+
+  find(:xpath,'//*[@id="signup-b"]')
+
+  find(:xpath, "//*[contains(text(), 'Contact Us')]/parent::section")
+
+  find(:xpath,'//*[@id="name"]')
+
+  find(:xpath,'//*[@id="email"]')
+
+  find(:xpath,'//*[@id="contactus-message"]')
+
+  find(:xpath,'//*[@id="contactus-button"]')
+
+  all(:xpath, "//*[@class='box special features']/descendant::section").each do |el|
     puts el
   end
+
+
   # all(:xpath, "//h3[contains(text(), 'Chain requests with reusable data')]").each do |el|
   #               # //*[@id="main"]/section[2]/div[1]/section[1]/img
 
